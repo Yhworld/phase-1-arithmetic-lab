68ce804a0f3d0e1a2d25f475472352c48a9a6380
Yahya <yahyahussein670@gmail.com>
2022-06-07 11:07:50 +0300

    commit tree 59d51ee49c03cfc602c326f6199e0e9addfda499

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+let multiply = num1 * num2;
+
+
+let random = Math.ceil(Math.random()*163760)
+
+console.log(random);
+
+const num3 = 24;
+const num4 = 5;
+
+let mod = num3 % num4 ;
+
+const max = Math.max(5, 10, 15, 20);
+
